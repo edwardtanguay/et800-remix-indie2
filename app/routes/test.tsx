a0889d7cab8117582e1cf33d71c2caeeba37e655
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-22 19:21:21 +0100

    added page

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,7 +1,8 @@
 export default function Index() {
   return (
     <section className="p-8">
-      <h1 className="text-3xl">Remix Site</h1>
+      <h1 className="text-3xl">Testing</h1>
+	  <p>this is a test</p>
     </section>
   );
 }
